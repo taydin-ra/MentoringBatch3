d035e0f00cf8700dd9a7f7674f00b2f0fa28ef40
taydin-ra <ttorun16@gmail.com>
2020-11-05 18:45:34 -0500

    Access modifier notes are pushed

--- src/accessModifiers/DifferentClass.java
+++ src/accessModifiers/DifferentClass.java
@@ -1,22 +1,34 @@
 package accessModifiers;
 
 public class DifferentClass {
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
 
 
-        Summary summary = new Summary();
+//        Summary summary = new Summary();
+//
+//        summary.age=56;
+//        summary.name="Aydin";
+//        summary.num=7.8;
+    // summary.isFriday=false;  it is not accessible in the different class
 
-        summary.age=56;
-        summary.name="Aydin";
-        summary.num=7.8;
-       // summary.isFriday=false;  it is not accessible in the different class
+
+    // Task
+    // 1. create a method named as stateTax,
+    // which is accessible just within the same package!
+    // return type is double, parameter is String.
+    // remove any $ and , signs from the String, convert into double and calculate state tax
+    // by getting the 7.75 percent of the double value.
 
 
-        // Task
-        // 1. create a method named as stateTax,
-        // which is accessible just within the same package!
-        // return type is double, parameter is String.
-        // remove any $ and , signs from the String, convert into double and calculate state tax
-        // by getting the 7.75 percent of the double value.
+   protected double stateTax(String num) {
+        num = num.replace("$", "");
+        num = num.replace(",", "");
+
+        double tax = Double.valueOf(num); // Double.parseDouble(num);
+
+        double result = tax * 0.0775;
+
+        return result;
+
     }
 }
--- src/anotherPackage/AnotherClass.java
+++ src/anotherPackage/AnotherClass.java
@@ -1,15 +1,21 @@
 package anotherPackage;
 
+import accessModifiers.DifferentClass;
 import accessModifiers.Summary;
 
-public class AnotherClass extends Summary { // child class
+public class AnotherClass extends DifferentClass { // child class
     public static void main(String[] args) {
         Summary summary=new Summary();
         summary.age=34;
 
         AnotherClass anotherClass=new AnotherClass();
-        anotherClass.age=23;
-        anotherClass.num=12.9;
+        double tax=anotherClass.stateTax("$5,6");
+
+        System.out.println(tax);
+
+
+
+
 
     }
 }
