380f48411bda8d7a35e89420663ad95988667dab
taydin-ra <ttorun16@gmail.com>
2020-11-13 18:48:14 -0500

    Last notes are added about string builder

--- src/stringBuilder/AlphabeTask.java
+++ src/stringBuilder/AlphabeTask.java
@@ -3,15 +3,20 @@ package stringBuilder;
 public class AlphabeTask {
     public static void main(String[] args) {
         // Create the alphabet by using StringBuilder
-        StringBuilder sb=new StringBuilder();
-        for (char i = 'a'; i <='z' ; i++) {
+        StringBuilder sb = new StringBuilder();
+        for (char i = 'a'; i <= 'z'; i++) {
             sb.append(i);
         }
         System.out.println(sb);
 
         // Create a method reverseStringBuilder
         // Return type is String, parameter is StringBuilder
-        // Reverse the stringbuilder
+        // Reverse the StringBuilder
         // convert to String
+    }
+        public String reverseStringBuilder(StringBuilder sb){
+
+        String result=sb.reverse().toString();
+        return result;
     }
 }
--- src/stringBuilder/Summary.java
+++ src/stringBuilder/Summary.java
@@ -8,21 +8,21 @@ public class Summary {
         Link
         https://docs.google.com/presentation/d/1FJxNjsimlU9ZVohMZeqY9gpMdkLNjPwob2LtdB9zPwI/edit#slide=id.p
          */
-        
+
         /*
         Normally equals method is looking for the location of the
         elements in the java memory, if they have the same spot or not.
         But String changes(override) this method according to it's needs
         It is checking the content of the element equality
          */
-        String s1="Cat";
+        String s1="Cat";  // String literal and located in String Pool
         String s2="Cat";
-        String s3=new String("Cat");
+        String s3=new String("Cat"); // it is created in heap memory
 
         System.out.println( s1.equals(s3));
         System.out.println( s1==s3);
 
-        String str = "a";
+        String str = "a";  // strings are immutable
         System.out.print("First location in the memory : ");
         System.out.println(str.hashCode());
         str = str.concat("b");
@@ -38,7 +38,7 @@ public class Summary {
 
         // StringBuilder
         System.out.println("--------------------");
-        StringBuilder sb = new StringBuilder("a");
+        StringBuilder sb = new StringBuilder("a");  // mutable
         System.out.println(sb.capacity());
         System.out.println("Memory of the stringbuilder: " + sb.hashCode());
         sb.append("b");
@@ -51,5 +51,12 @@ public class Summary {
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder("atlanta");
         StringBuilder sb3 = new StringBuilder(32); // by default 16
+
+        // StringBuffer
+        // convert StringBuilder to StringBuffer
+
+        StringBuffer sf= new StringBuffer(sb2);
+        System.out.println(sf);
+
     }
 }
